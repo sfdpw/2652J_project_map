9bd293ddfb8060e8fa9ea6cbc58698128fe50418
bulklc <clog@ucdavis.edu>
2020-12-02 21:31:12 -0800

    modified:   css/leaflet.css 	new file:   data/200_laterals_2.js 	new file:   data/201_MHs_5.js 	new file:   data/202_SW_mains_0.js 	new file:   data/203_drains_4.js 	new file:   data/204_culverts_3.js 	new file:   data/205_shear_bands_1.js 	modified:   index.html 	new file:   legend/200_laterals_2_EtoRemain0.png 	new file:   legend/200_laterals_2_ExtendandConnectEorReplacewithNasDirectedbyCity1.png 	new file:   legend/200_laterals_2_IntercepttoConnectEorReplacewithNasDirectedbyCity2.png 	new file:   legend/200_laterals_2_RemoveE6.png 	new file:   legend/200_laterals_2_ReplaceEwithN3.png 	new file:   legend/200_laterals_2_ReplaceEwithNExtendandConnect5.png 	new file:   legend/200_laterals_2_ReplaceEwithNasDirectedbyCity4.png 	new file:   legend/201_MHs_5_AbandonE8.png 	new file:   legend/201_MHs_5_EtoRemain0.png 	new file:   legend/201_MHs_5_InstallNperSFPWSTDPlan871811.png 	new file:   legend/201_MHs_5_InstallNperSFPWSTDPlan871822.png 	new file:   legend/201_MHs_5_MortarE3.png 	new file:   legend/201_MHs_5_RemoveE7.png 	new file:   legend/201_MHs_5_ReplaceEwithNperSFPWSTDPlan871814.png 	new file:   legend/201_MHs_5_ReplaceEwithNperSFPWSTDPlan871825.png 	new file:   legend/201_MHs_5_ReplaceEwithangledNperSFPWSTDPlan871826.png 	new file:   legend/202_SW_mains_0_EtoRemain0.png 	new file:   legend/202_SW_mains_0_InstallNVCP3.png 	new file:   legend/202_SW_mains_0_LineEwithCIPL1.png 	new file:   legend/202_SW_mains_0_PlugandFillE2.png 	new file:   legend/202_SW_mains_0_RemoveE5.png 	new file:   legend/202_SW_mains_0_ReplaceEwithNVCP4.png 	new file:   legend/203_drains_4_AbandonE1.png 	new file:   legend/203_drains_4_EtoRemain0.png 	new file:   legend/203_drains_4_InstallNperSFPWSTDPlan871872.png 	new file:   legend/203_drains_4_InstallNperSFPWSTDPlan871883.png 	new file:   legend/203_drains_4_InstallNperSFPWSTDPlan871894.png 	new file:   legend/204_culverts_3_EtoRemain0.png 	new file:   legend/204_culverts_3_InstallNDIP1.png 	new file:   legend/204_culverts_3_InstallNVCP2.png 	new file:   legend/204_culverts_3_PlugE3.png 	new file:   legend/204_culverts_3_ReplaceEwithNasDirectedbyCity4.png 	new file:   legend/205_shear_bands_1.png

---
+++ data/205_shear_bands_1.js
@@ -0,0 +1,16 @@
+var json_205_shear_bands_1 = {
+"type": "FeatureCollection",
+"name": "205_shear_bands_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ -122.475098191706792, 37.724323323558984 ] } },
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "Point", "coordinates": [ -122.475093476656241, 37.724427884129099 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -122.476622395520167, 37.758464570561344 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -122.476623368443853, 37.758478261623466 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -122.476636606533603, 37.758678390923357 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -122.476637672628854, 37.758693735450123 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -122.477016375032164, 37.761873035866451 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -122.477076804067622, 37.762666921564872 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -122.475147508936388, 37.736290329838397 ] } }
+]
+}

0476ff12543abbf5900c90fdcaf9fb6a08f5be22
bulklc <clog@ucdavis.edu>
2021-10-12 14:13:03 -0700

    modified:   data/100_L_layout_319.js 	new file:   tcps/2652J_TCP_162_grind_and_pave_noriega.pdf 	new file:   tcps/2652J_TCP_163_grind_and_pave_moraga.pdf 	new file:   tcps/2652J_TCP_164_grind_and_pave_lawton.pdf 	new file:   tcps/2652J_TCP_165_grind_and_pave_kirkham.pdf 	new file:   tcps/2652J_TCP_166_grind_and_pave_irving.pdf 	modified:   tcps/tcp_list.js

--- tcps/tcp_list.js
+++ tcps/tcp_list.js
@@ -150,5 +150,11 @@ var json_tcp_list = [
 '2652J_TCP_143_concrete_sloat',
 '2652J_TCP_146_sewer_lincoln_to_irving',
 '2652J_TCP_147_sewer_wawona_to_sloat',
-'2652J_TCP_149_wawona'
+'2652J_TCP_149_wawona',
+'2652J_TCP_162_grind_and_pave_noriega',
+'2652J_TCP_163_grind_and_pave_moraga',
+'2652J_TCP_164_grind_and_pave_lawton',
+'2652J_TCP_165_grind_and_pave_kirkham',
+'2652J_TCP_166_grind_and_pave_irving'
+
 ]

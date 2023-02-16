ea13c9940ccdfc5d3ca0d99a74367cf789dfaff2
chris <clog@ucdavis.edu>
2023-02-15 20:11:44 -0800

    modified:   data/400_flatwork.js 	modified:   data/548_E_TS_cabinets.js 	modified:   data/base_sov.js

--- data/548_E_TS_cabinets.js
+++ data/548_E_TS_cabinets.js
@@ -22,7 +22,7 @@ var json_548_E_TS_cabinets = {
 
 { "type": "Feature", "properties": { "install_id": "E_52_E_02_001", "location": "Winston Dr Intersection - NEC", 'STATUS': "Post-Construction", 'PP_HISTORY': { "E-52": { "PP24": { "SFMTA": { "QTY": 1, "UNIT": "EA" } } } }, "rlvnt": "none" }, "geometry": { "type": "Point", "coordinates": [ -122.474607313956568, 37.727008733553703 ] } },
 
-{ "type": "Feature", "properties": { "install_id": "E_52_E_05_001", "location": "Sloat Blvd Intersection - NEC", 'STATUS': "Post-Construction", 'PP_HISTORY': { "E-52": { "PP25": { "SFMTA": { "QTY": 1, "UNIT": "EA" } } } }, "rlvnt": "none" }, "geometry": { "type": "Point", "coordinates": [ -122.474943715981354, 37.734803216331109 ] } }
+//{ "type": "Feature", "properties": { "install_id": "E_52_E_05_001", "location": "Sloat Blvd Intersection - NEC", 'STATUS': "Post-Construction", 'PP_HISTORY': { "E-52": { "PP25": { "SFMTA": { "QTY": 1, "UNIT": "EA" } } } }, "rlvnt": "none" }, "geometry": { "type": "Point", "coordinates": [ -122.474943715981354, 37.734803216331109 ] } }
 
 
 ]

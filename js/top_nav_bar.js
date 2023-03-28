8dad947cc57a879d1fbab75484b3ae164a594610
chris <clog@ucdavis.edu>
2023-03-28 16:25:16 -0700

    modified:   data/base_sov.js 	new file:   funding/funding_PP27.html 	modified:   js/top_nav_bar.js 	new file:   sov/sov_PP27.html

--- js/top_nav_bar.js
+++ js/top_nav_bar.js
@@ -97,9 +97,8 @@ function top_nav(nav_element, pp_no = null, bid_item_id = null)
 	                  <ul class="submenu dropdown-menu">\
 	                    <li>\
 	      		      <a class="dropdown-item" href="' + nav_path + 'sov/sov_PP25.html">PP25 - Dec 2022 / Jan 2023</a>\
-	      		    </li>\
-	      		    <li>\
 	      		      <a class="dropdown-item" href="' + nav_path + 'sov/sov_PP26.html">PP26 - Feb 2023</a>\
+	      		      <a class="dropdown-item" href="' + nav_path + 'sov/sov_PP27.html">PP27 - Mar 2023</a>\
 	      		    </li>\
 	      		  </ul>\
 	              </li>\
@@ -157,10 +156,9 @@ function top_nav(nav_element, pp_no = null, bid_item_id = null)
                         <a class="dropdown-item" href="#">2023 &raquo;</a>\
 	                  <ul class="submenu dropdown-menu">\
 	                    <li>\
-	                      <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP25.html">PP25 - Dec 2022 / Jan 2023</a>\
-	                    </li>\
-	                    <li>\
+	           <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP25.html">PP25 - Dec 2022 / Jan 2023</a>\
 	                      <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP26.html">PP26 - Feb 2023</a>\
+	                      <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP27.html">PP27 - Mar 2023</a>\
 	                    </li>\
 	                  </ul>\
 	              </li>\
